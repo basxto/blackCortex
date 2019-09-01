e7ab6d4dbcb8fc013e6acfc8a081e97d08051d0f
Sebastian Riedel <git@basxto.de>
2019-09-01 19:13:11 +0200

    Stub NativeCrabAI

---
+++ Base.rte/Actors/AI/NativeCrabAI.lua
@@ -0,0 +1,34 @@
+NativeCrabAI = {}
+
+function NativeCrabAI:Create(Owner)
+end
+
+function NativeCrabAI:Update(Owner)
+end
+
+function NativeCrabAI:Destroy(Owner)
+end
+
+function NativeCrabAI:CreatePatrolBehavior(Owner)
+end
+
+function NativeCrabAI:CreateSentryBehavior(Owner)
+end
+
+function NativeCrabAI:CreateAttackBehavior(Owner)
+end
+
+function NativeCrabAI:CreateSuppressBehavior(Owner)
+end
+
+function NativeCrabAI:CreateGoToBehavior(Owner)
+end
+
+function NativeCrabAI:CreateBrainSearchBehavior(Owner)
+end
+
+function NativeCrabAI:CreateFaceAlarmBehavior(Owner)
+end
+
+function NativeCrabAI:CreatePinBehavior(Owner)
+end
