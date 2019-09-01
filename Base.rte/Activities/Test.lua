4d7dba9e0302cd587c320e26adf00bba99637413
Sebastian Riedel <git@basxto.de>
2019-09-01 18:34:17 +0200

    Improve test activity
    Can now play physics test

--- Base.rte/Activities/Test.lua
+++ Base.rte/Activities/Test.lua
@@ -2,6 +2,17 @@ package.loaded.Constants = nil; require("Constants");
 
 function Test:StartActivity()
 	print("Test:StartActivity()");
+
+	for p = Activity.PLAYER_1, Activity.MAXPLAYERCOUNT - 1 do
+		if not self:GetPlayerBrain(p) then
+			self.ActivityState = Activity.EDITING
+			-- For reachability checks
+			MovableMan:OpenAllDoors(true, Activity.NOTEAM)
+			AudioMan:ClearMusicQueue()
+			-- Play editor music
+			AudioMan:PlayMusic("Base.rte/Music/dBSoundworks/ccambient4.ogg", -1, -1)
+		end
+	end
 end
 
 function Test:PauseActivity(pause)
